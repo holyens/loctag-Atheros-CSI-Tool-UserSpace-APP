abb4fe3345bebc2ca103426c182e82ee77f8f1c8
holyens <644026981@qq.com>
2021-02-13 21:49:02 +0800

    modify configure for loctag

--- hostapd-2.5/hostapd/start_hostapd.sh
+++ hostapd-2.5/hostapd/start_hostapd.sh
@@ -3,8 +3,8 @@
 sudo nmcli nm wifi off
 sudo rfkill unblock wlan
 
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 10.10.0.1/24 up
+sudo ifconfig wlan1 down
+sudo ifconfig wlan1 10.10.0.1/24 up
 sleep 1
 
 sudo service isc-dhcp-server restart
